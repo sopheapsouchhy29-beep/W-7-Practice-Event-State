244965c2091f7562481b6669e0691ced533ac92c
sopheapsouchhy29-beep <sopheapsouchhy29@gmail.com>
2026-03-12 22:06:44 +0700

    Finished Exercise-4

--- exercise-4/src/components/Carousel.jsx
+++ exercise-4/src/components/Carousel.jsx
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 
 export const Carousel = ({ images }) => {
-  /* You will need to  use  state to mnage the current image */
+  /* You will need to use state to manage the current image */
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  /* You will need to hanle the click on left and right button */
+  /* You will need to handle the click on left and right button */
+  function goToPrevious() {
+    /* You will need to manage the cases when we are on the last image or first image*/
+    const isFirstImage = currentIndex === 0;
+    const newIndex = isFirstImage ? images.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  }
 
-  /* You will need to manage the cases when we are on the last image or first image*/
+  function goToNext() {
+    /* You will need to manage the cases when we are on the last image or first image*/
+    const isLastImage = currentIndex === images.length - 1;
+    const newIndex = isLastImage ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  }
 
   return (
     <div className="carousel">
-      <BsArrowLeftCircleFill className="arrow arrow-left" />
+      <BsArrowLeftCircleFill 
+        className="arrow arrow-left" 
+        onClick={goToPrevious}
+      />
 
-      {/* YOu will need to display the current image, not the first one.. */}
-      <img src={images[0].src} alt={images[0].alt} className="slide" />
+      {/* You will need to display the current image, not the first one.. */}
+      <img 
+        src={images[currentIndex].src} 
+        alt={images[currentIndex].alt} 
+        className="slide" 
+      />
 
-      <BsArrowRightCircleFill className="arrow arrow-right" />
+      <BsArrowRightCircleFill 
+        className="arrow arrow-right" 
+        onClick={goToNext}
+      />
     </div>
   );
 };
